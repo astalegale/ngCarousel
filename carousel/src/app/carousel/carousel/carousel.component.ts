b576c5243e5a3ea119f93a69a447531737942fd4
Alex <a.cantu@astalegale.net>
2017-10-04 01:09:39 +0200

    esercizio finito

--- carousel/src/app/carousel/carousel/carousel.component.ts
+++ carousel/src/app/carousel/carousel/carousel.component.ts
@@ -1,44 +1,54 @@
-import { Component, OnInit, AfterContentInit, ContentChildren, QueryList, Input } from '@angular/core';
-import { ItemComponent } from '../item/item.component';
+import {
+  Component,
+  OnInit,
+  AfterContentInit,
+  ContentChildren,
+  QueryList,
+  Input
+} from "@angular/core";
+import { ItemComponent } from "../item/item.component";
 
 @Component({
-    selector: 'app-carousel',
-    templateUrl: './carousel.component.html',
-    styleUrls: ['./carousel.component.css']
+  selector: "app-carousel",
+  templateUrl: "./carousel.component.html",
+  styleUrls: ["./carousel.component.css"]
 })
 export class CarouselComponent implements OnInit, AfterContentInit {
+  @Input() delay: number;
 
-    @Input() delay: number;
+  imgCount: number;
+  imgIdx = 0;
 
-    imgCount: number;
-    imgIdx = 0;
+  @ContentChildren(ItemComponent) itemList: QueryList<ItemComponent>;
+  constructor() {}
 
-    @ContentChildren(ItemComponent) itemList: QueryList<ItemComponent>;
-    constructor() { }
+  delays(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 
-    async delays(ms: number) {
-        return new Promise(resolve => setTimeout(resolve, ms));
+  async startCarousel() {
+    let curImgidx;
+    let prevImgidx;
+    while (true) {
+        console.log("idx",this.imgIdx);
+      prevImgidx = this.imgIdx == 0 ? 0 : (this.imgIdx-1) % this.imgCount;
+      curImgidx = this.imgIdx % this.imgCount;
+
+      console.log("cur", curImgidx, "prev", prevImgidx);
+
+      console.log(this.itemList.toArray()[curImgidx]);
+      this.itemList.toArray()[prevImgidx].isHide = true;
+      this.itemList.toArray()[curImgidx].isHide = false;
+      this.imgIdx++;
+      await this.delays(this.delay);
     }
+  }
 
-    startCarousel() {
-        let curImgidx;
-        let prevImgidx;
-        while (true) {
+  ngOnInit() {}
 
-            curImgidx = this.imgIdx % this.imgCount;
-            prevImgidx = this.imgIdx % this.imgCount;
-            await this.delays(this.delay);
+  ngAfterContentInit(): void {
+    this.imgCount = this.itemList.length;
 
-        }
-    }
-
-    ngOnInit() {
-    }
-
-    ngAfterContentInit(): void {
-
-        this.imgCount = this.itemList.length;
-
-        this.startCarousel();
-    }
+    this.startCarousel();
+  }
 }
